05a2289c7c4f83622a32d4fc3f3f2498f9cb6018
sakshigujar26 <2022bit@sggs.ac.in>
2024-01-28 22:49:20 +0530

    AddSubFunction

---
+++ AddSubFun.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Calci{
+ public static double Add( double num1,double num2){
+   double result=num1+num2;
+   return result;
+   }
+  public static double Sub(double num1,double num2){
+   double result=num1-num2;
+   return result;
+  }
