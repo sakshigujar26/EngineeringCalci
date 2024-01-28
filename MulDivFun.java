b220571154876d9761f64e84f6da52b870fcd7dc
sakshigujar26 <2022bit@sggs.ac.in>
2024-01-28 22:52:22 +0530

    MulDivFunction

---
+++ MulDivFun.java
@@ -0,0 +1,14 @@
+public static double Multiplication(double num1,double num2){
+   double result=num1*num2;
+   return result;
+   }
+  public static double Division(double num1,double num2){
+  if(num2 != 0){
+  double result = num1/num2;
+  return result;
+  }
+  else{
+  return -1;
+  }
+  }
+  
