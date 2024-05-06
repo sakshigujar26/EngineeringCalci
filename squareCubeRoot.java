6da118e704bcde6e9f0f17d4d25f339a1dab9303
sakshigujar26 <2022bit@sggs.ac.in>
2024-05-06 23:56:50 +0530

    fun4

---
+++ squareCubeRoot.java
@@ -0,0 +1,9 @@
+ public static double squareRoot(double num1) {
+        if (num1 >= 0) {
+            return Math.sqrt(num1);        // sq.root
+        } }
+
+    public static double cubeRoot(double num1) {
+        return Math.cbrt(num1);        //cuberoot
+    }
+
