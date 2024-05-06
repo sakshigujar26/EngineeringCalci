dc7c670154699a7d5983bc186e51b47f2779dbca
sakshigujar26 <2022bit@sggs.ac.in>
2024-05-06 23:55:10 +0530

    fun3

---
+++ squareCube.java
@@ -0,0 +1,7 @@
+public static double square(double num1) {
+        return num1 * num1;    //square
+    }
+
+    public static double cube(double num1) {
+        return num1 * num1 * num1;      //cube
+    }
